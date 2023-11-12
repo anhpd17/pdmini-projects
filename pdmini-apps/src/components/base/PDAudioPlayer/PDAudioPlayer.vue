f0b26e1e234fa43cef727339b832aa88cc7eff47
pdanh <anhpd17@gmail.com>
2023-11-12 17:01:27 +0700

    feat: Thêm audio player và chuyển lùi bài hát

---
+++ pdmini-apps/src/components/base/PDAudioPlayer/PDAudioPlayer.vue
@@ -0,0 +1,37 @@
+<template>
+    <audio-player
+        :audio-list="props.audioList.map((elm) => elm.url)"
+        :before-play="props.handleBeforePlay"
+        :before-next="props.handleBeforeNext"
+        :before-prev="props.handleBeforePrev"
+        :theme-color="props.themeColor"
+    />
+</template>
+<script setup>
+import AudioPlayer from "@liripeng/vue-audio-player";
+const props = defineProps({
+    audioList: {
+        type: Array,
+        required: true,
+    },
+    handleBeforePlay: {
+        type: Function,
+        required: true,
+    },
+    handleBeforeNext: {
+        type: Function,
+        required: true,
+    },
+    handleBeforePrev: {
+        type: Function,
+        required: true,
+    },
+    themeColor: {
+        type: String,
+        default: "#ff2929",
+    },
+});
+</script>
+<style>
+@import url("./PDAudioPlayer.css");
+</style>
--- pdmini-apps/src/views/Music/Music.vue
+++ pdmini-apps/src/views/Music/Music.vue
@@ -1,5 +1,55 @@
-<template lang="">
-  <main id="main-music">music</main>
+<template>
+    <main id="main-music">
+        <div id="music-player">
+            <div class="player-info">
+                <h2 class="player-title">{{ playingSong.name }}</h2>
+                <h4 class="player-author">{{ playingSong.author }}</h4>
+            </div>
+            <div class="player-thumbnail">
+                <img
+                    id="player-thumbnail-img"
+                    :src="playingSong.links.images[0].url"
+                    alt=""
+                />
+            </div>
+            <PDAudioPlayer
+                :audioList="audioList"
+                themeColor="#1DB954"
+                :handleBeforePlay="handleBeforePlay"
+                :handleBeforeNext="handleBeforeNext"
+                :handleBeforePrev="handleBeforePrev"
+            />
+        </div>
+        <div id="music-queue">sd</div>
+    </main>
 </template>
-<script></script>
-<style></style>
+<script setup>
+import { ref } from "vue";
+import songs from "../../assets/data/songs.json";
+import PDAudioPlayer from "../../components/base/PDAudioPlayer/PDAudioPlayer.vue";
+const audioList = songs;
+const playingSong = ref(audioList[0]);
+const isPlaying = ref(false);
+
+function handleBeforePlay(next) {
+    next();
+}
+
+function handleBeforeNext(next) {
+    let currentIndex = playingSong.value.id;
+    let nextIndex =
+        currentIndex + 1 == audioList.length ? currentIndex : currentIndex + 1;
+    playingSong.value = audioList[nextIndex];
+    next();
+}
+
+function handleBeforePrev(next) {
+    let currentIndex = playingSong.value.id;
+    let nextIndex = currentIndex - 1 < 0 ? 0 : currentIndex - 1;
+    playingSong.value = audioList[nextIndex];
+    next();
+}
+</script>
+<style>
+@import url("./Music.css");
+</style>
