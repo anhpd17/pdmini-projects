ebc22cecf24cdc3448f5dfcd33bc2523381a5565
pdanh <anhpd17@gmail.com>
2023-11-13 22:02:26 +0700

    feat: hoàn thiện chức năng trang music và chỉnh sửa data bài hát

--- pdmini-apps/src/components/base/PDAudioPlayer/PDAudioPlayer.vue
+++ pdmini-apps/src/components/base/PDAudioPlayer/PDAudioPlayer.vue
@@ -1,14 +1,18 @@
 <template>
     <audio-player
+        ref="audioPlayerRef"
         :audio-list="props.audioList.map((elm) => elm.url)"
         :before-play="props.handleBeforePlay"
         :before-next="props.handleBeforeNext"
         :before-prev="props.handleBeforePrev"
         :theme-color="props.themeColor"
+        @pause="props.onPause()"
     />
 </template>
 <script setup>
+import { ref } from "vue";
 import AudioPlayer from "@liripeng/vue-audio-player";
+const audioPlayerRef = ref(null);
 const props = defineProps({
     audioList: {
         type: Array,
@@ -26,11 +30,23 @@ const props = defineProps({
         type: Function,
         required: true,
     },
+    onPause: {
+        type: Function,
+        required: true,
+    },
     themeColor: {
         type: String,
         default: "#ff2929",
     },
 });
+function play() {
+    audioPlayerRef.value.play();
+}
+function pause() {
+    props.onPause();
+    audioPlayerRef.value.play();
+}
+defineExpose({ play, pause });
 </script>
 <style>
 @import url("./PDAudioPlayer.css");
